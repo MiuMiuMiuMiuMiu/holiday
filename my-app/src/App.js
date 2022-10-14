a9103f65036a96ea1b386ed60668ea765016679c
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-10-14 13:33:08 +0200

    Create navigation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import Home from "./pages/home";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <ThemeProvider
+        breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+        minBreakpoint="xxs">
+        <div className="App">
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand as={Link} to="/">Holidays</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#link">Folder</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+      </ThemeProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
