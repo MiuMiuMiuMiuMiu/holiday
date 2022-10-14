b2d52a1623e13983e362bd7edcafea513838af05
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-10-14 14:01:29 +0200

    Display component in home-page

---
+++ my-app/src/components/Holidays.js
@@ -0,0 +1,10 @@
+//import axios from 'axios';
+function Holidays() {
+    return (
+       <div>
+            <h1>I SHOW UP!</h1>
+       </div> 
+    )
+}
+
+export default Holidays;
--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Holidays from '../components/Holidays.js';
 
 function Home() {
     return (
         <div>
-            <h1>HELLO????</h1>
+            < Holidays/>
         </div>
     )
 }
