5c28c44e461b90e454e4ec164349cd6f1f483510
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-10-19 10:34:34 +0200

    Created search bar component

---
+++ my-app/src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SearchBar() {
+
+    console.log("This is the search bar component")
+
+    return (
+       <div>
+            {listItems}
+       </div> 
+    )
+}
+
+export default SearchBar;
