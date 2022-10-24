3921fa6e291feda79adf28e03d187fd001f48305
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2022-10-24 09:22:28 +0200

    Search bar click - open country selection

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Countries from '../components/Countries.js';
-
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -8,6 +7,8 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
 function SearchBar() {
+
+    const [active, setActive] = useState(false);
 
     console.log("This is the search bar component")
 
@@ -17,8 +18,10 @@ function SearchBar() {
             <Form size="lg">
                 <Row className="justify-content-center"> 
                     <Col xs={5}>
-                        <Form.Control size="lg" type="text" placeholder="Country" />
-                        {< Countries/>}
+                        <Form.Control onClick={() => setActive(true)} size="lg" type="text" placeholder="Country" />
+                        {active === true &&
+                            < Countries/>
+                        }
                     </Col>
                     <Col xs="auto">
                         <Button variant="info" size="lg" type="submit">Show</Button>
