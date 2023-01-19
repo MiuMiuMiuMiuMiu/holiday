69d1b9862b5d57225b05d1e0abccdcffc7ac3a13
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2023-01-19 16:37:27 +0100

    Remove component Countries.js

--- my-app/src/pages/country.js
+++ my-app/src/pages/country.js
@@ -1,7 +1,4 @@
 import React from 'react';
-//import Holidays from '../components/Holidays.js';
-//import Countries from '../components/Countries.js';
-import SearchBar from '../components/SearchBar.js';
 
 function Home() {
     return (
--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -6,8 +6,6 @@ import SearchBar from '../components/SearchBar.js';
 function Home() {
     return (
         <div className="search-bar">
-            {/*< Holidays/>*/}
-            {/*< Countries/>*/}
             {< SearchBar/>}
         </div>
     )
